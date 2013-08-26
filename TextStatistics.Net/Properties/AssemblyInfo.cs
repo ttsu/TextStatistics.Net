bfd3a0ea4d8482e11fe738a898d1da7882548334
Tim Tsu <timtsu@gmail.com>
2013-08-25 21:28:48 -0500

    fix project

--- TextStatistics.Net/Properties/AssemblyInfo.cs
+++ TextStatistics.Net/Properties/AssemblyInfo.cs
@@ -14,6 +14,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00afc087-9ba5-414a-89d9-8bee2a04d3af")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -26,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
